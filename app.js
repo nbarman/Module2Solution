c9aa4f9d5c4dcd9e886b6def1305fe4cb6abd425
nbarman <b.namit87@gmail.com>
2017-01-23 13:12:32 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,83 @@
+(function () {
+'use strict';
+//nbarman
+angular.module('ShoppingListCheckOff', [])
+.controller('AddToListController', AddToListController )
+.controller('ToBuyListController', ToBuyListController )
+.provider('ShoppingListItemsService', ShoppingListItemsServiceProvider);
+
+var itemsList = [
+
+        {
+          name : "Milk", quantity: 10
+        },
+        {
+          name : "Cookies", quantity: 5
+        },
+        {
+          name : "Chips", quantity: 4
+        },
+        {
+          name : "Wafers", quantity: 6
+        },
+        {
+          name : "Bismol", quantity: 8
+        },
+      ];
+
+//Controller#1 ToBuyListController
+ToBuyListController.$inject = ['$scope', 'ShoppingListItemsService'];
+function ToBuyListController($scope, ShoppingListItemsService){
+    var toBuyCtrl = this;
+    toBuyCtrl.items = ShoppingListItemsService.getItemsOnList();
+  toBuyCtrl.addToBoughtList = function(index){
+    ShoppingListItemsService.itemBought(index);
+  };
+}
+
+
+// Controller#2 AddToListController
+AddToListController.$inject = ['$scope', 'ShoppingListItemsService'];
+function AddToListController($scope, ShoppingListItemsService){
+var itemsAdded = this;
+itemsAdded.items = ShoppingListItemsService.getItemsBought();
+
+}
+
+
+//Service
+function ShoppingListItemsService(items){
+
+  var service  = this;
+  var itemsBought = [];
+  service.itemBought = function(index){
+      var item = {name : items[index].name, quantity : items[index].quantity};
+      //console.log(item);
+      itemsBought.push(item);
+      items.splice(index,1);
+    };
+
+  service.getItemsBought = function(){
+    return itemsBought;
+  };
+
+  service.getItemsOnList = function(){
+    return items;
+  };
+
+}
+
+//Provider
+function ShoppingListItemsServiceProvider(){
+
+  var provider = this;
+  provider.items = itemsList;
+
+        provider.$get = function(){
+        var shoppingListItems = new ShoppingListItemsService(provider.items);
+        return   shoppingListItems;
+      };
+
+}
+
+})();
